8e1471fbe53cf64c24e2364803c598f0f0593e90
Tanvir <Tanvirislamnyc@gmail.com>
2025-11-12 22:52:54 -0500

    use client fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SignIn } from '@clerk/nextjs';
 import Logo from '@/components/Logo';
 
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SignUp } from '@clerk/nextjs';
 import Logo from '@/components/Logo';
 
